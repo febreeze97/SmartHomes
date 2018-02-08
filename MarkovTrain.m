d5a0be93c03ce937267f0d453230ac70b0a66408
James <jr15832@my.bristol.ac.uk>
2018-02-08 19:46:12 +0000

    Got HMM testing to work

--- MarkovTrain.m
+++ MarkovTrain.m
@@ -10,7 +10,7 @@ Trans = [0.7, 0.15,   0.01, 0.14;
          0.3,  0.3, 0.3,  0.1];
      
 %Emm = eye(4); % 0.25*ones(4,4);
-Right = 0.6;
+Right = 1;
 Emm = Right*eye(4) + ((1-Right)/4)*ones(4,4);
 
 [ESTTR,ESTEMIT] = hmmtrain(RoomIndex,Trans,Emm)
